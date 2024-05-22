7d40bd46ce484acea31f47fb30a12acea1654311
Cho JeongTaek <cjt3591@gmail.com>
2024-05-22 13:03:23 +0900

    Chore: 스타일 테마 설정

---
+++ src/commons/styles/theme.ts
@@ -0,0 +1,12 @@
+const theme = {
+	colors: {
+		primary: '#19ED7B',
+		secondary: '#6c757d',
+		success: '#28a745',
+		danger: '#dc3545',
+		warning: '#ffc107',
+		background: '#ffffff',
+	},
+};
+
+export default theme;
