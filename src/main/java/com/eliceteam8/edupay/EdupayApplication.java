8bd3873e68e2ff661cf20ca6426a7ee24e4deac4
jeongjiyong <work.jjy1262@gmail.com>
2024-05-24 00:39:52 +0900

    base

---
+++ src/main/java/com/eliceteam8/edupay/EdupayApplication.java
@@ -0,0 +1,13 @@
+package com.eliceteam8.edupay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdupayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdupayApplication.class, args);
+	}
+
+}
