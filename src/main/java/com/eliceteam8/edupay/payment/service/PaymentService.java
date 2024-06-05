2642c57fc5be165eacc697b3c307d608b15f3db1
jeongsu <qaknc8@gmail.com>
2024-06-05 18:53:59 +0900

    feat: 결제 상태 변경

--- src/main/java/com/eliceteam8/edupay/payment/service/PaymentService.java
+++ src/main/java/com/eliceteam8/edupay/payment/service/PaymentService.java
@@ -1,9 +1,11 @@
 package com.eliceteam8.edupay.payment.service;
 
 import com.eliceteam8.edupay.bill.domain.Bill;
+import com.eliceteam8.edupay.bill.domain.Status;
 import com.eliceteam8.edupay.bill.repository.BillRepository;
 import com.eliceteam8.edupay.payment.dto.CallbackRequestDTO;
 import com.eliceteam8.edupay.payment.dto.PaymentInfoDTO;
+import com.eliceteam8.edupay.payment.entity.PaymentStatus;
 import com.siot.IamportRestClient.IamportClient;
 import com.siot.IamportRestClient.exception.IamportResponseException;
 import com.siot.IamportRestClient.response.IamportResponse;
@@ -40,6 +42,10 @@ public class PaymentService {
             IamportResponse<Payment> iamportResponse = getIamportResponse(callbackRequestDTO);
             Bill bill = billRepository.findByBillId(callbackRequestDTO.getBillId()).orElseThrow(NoSuchElementException::new);
             validatePaymentStatusAndPay(iamportResponse, bill);
+
+            if (!Status.PAID.equals(bill.getStatus())) {
+                bill.setStatusToPaid();
+            }
 
             return iamportResponse;
 
