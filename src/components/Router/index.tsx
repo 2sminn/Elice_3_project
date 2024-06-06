cdc4abbf901f72e1473ab159bc59178c1e772076
Cho JeongTaek <cjt3591@gmail.com>
2024-06-07 00:02:48 +0900

    Feat: 포트원 결제/성공,실패 페이지

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -4,6 +4,7 @@ import { LAYOUT_ROUTE_ARR, ROUTE_ARR } from '../../constants/routeList';
 import PublicRoute from './PubliceRoute';
 import NotFound from './NotFound';
 import BillPage from '../../pages/bill';
+import BillCompletePage from '../../pages/bill/complete';
 
 const RouterComponent = () => {
 	return (
@@ -21,6 +22,8 @@ const RouterComponent = () => {
 					))}
 				</Route>
 				<Route path="/bill/:id" element={<BillPage />} />
+				<Route path="/bill/success" element={<BillCompletePage isSuccess />} />
+				<Route path="/bill/error" element={<BillCompletePage />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</Router>
