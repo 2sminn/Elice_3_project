fb55d8d8b52544de6ddf3e8121ba06302a1bec0c
[Cloud] 이성민 <sungmingod13@gmail.com>
2024-06-08 11:33:05 +0900

    status refactoring

--- src/main/java/com/eliceteam8/edupay/get_cost/dto/StudentPaymentStatusResponseDto.java
+++ src/main/java/com/eliceteam8/edupay/get_cost/dto/StudentPaymentStatusResponseDto.java
@@ -1,5 +1,6 @@
 package com.eliceteam8.edupay.get_cost.dto;
 
+import com.eliceteam8.edupay.bill.domain.Status;
 import com.eliceteam8.edupay.get_cost.entity.StudentPaymentStatus;
 import lombok.Builder;
 import lombok.Data;
@@ -13,7 +14,7 @@ public class StudentPaymentStatusResponseDto {
     private Long studentId;
     private Long billId;
     private Long paymentId;
-    private String billStatus;
+    private Status billStatus;
     private LocalDateTime updatedAt;
 
     public static StudentPaymentStatusResponseDto fromEntity(StudentPaymentStatus entity) {
@@ -24,7 +25,7 @@ public class StudentPaymentStatusResponseDto {
                 .studentId(entity.getStudent().getId())
                 .billId(entity.getBill().getId())
                 .paymentId(entity.getPayment().getId())
-                .billStatus(entity.getBill().getStatus().name())
+                .billStatus(entity.getBill().getStatus())
                 .updatedAt(entity.getUpdatedAt())
                 .build();
     }
